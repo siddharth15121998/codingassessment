65586ffa123ba306c0d83c056a2a56bc0b89f3f5
siddharth15121998 <mastersiddharthgupta@gmail.com>
2021-07-24 10:24:24 +0530

    question1

---
+++ questions.py
@@ -0,0 +1,3 @@
+print("1. Create 2 lists named “even” & “Odd. Then read a number from the user and check whether it s an even or odd number by using user defined function “check” and store it to the orresponding list and display it")
+
+print(" 2. Read the string “INDIA IS MY COUNTRY” from the user and slice the word “INDIA” and send that sliced word to your email ")
